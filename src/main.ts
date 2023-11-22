75f010725d862288984201b1a326fbccfcbfce10
quirkles <al.quirk@gmail.com>
2023-11-22 10:55:09 -0500

    init first mutation

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const PORT = process.env['PORT'] || '3030';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(Number(3030));
 }
 bootstrap().then(() =>
